8b1e8760ead1efdcb24d9aae51673c092b4f5be3
Lucas Abreu <lucasabreuapi@gmail.com>
2020-03-02 01:40:14 -0300

    new changes 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,54 @@
 import React from 'react'
 import './Button.css'
 
+
+/* 
+
+    Exibicao condicional de classe 
+    eu aplico a classe OU NAO dentro no botão
+    de acordo com as PROPRIEDADES que eu recebi
+
+*/
 export default props => {
 
+    let classes = 'button '
+    classes +=
 
-    let classes = 'button';
-    classes += props.operation ? 'operation': 
-        
-        props.double ? 'double': 
-        props.triple ? 'triple': ''
+return (
 
-
-    return(
-
-    <button
-        onClick={e => props.click && props.click(props.label)}
-        className={classes}>
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double': ''}
+        ${props.triple ? 'triple': ''}
+    `}>
         {props.label}
     </button>
 
     )
+
+
 }
+
+
+
+// export default props => {
+
+
+//     let classes = 'button';
+//     classes += props.operation ? 'operation': 
+        
+//         props.double ? 'double': 
+//         props.triple ? 'triple': ''
+
+
+//     return(
+
+//     <button
+//         onClick={e => props.click && props.click(props.label)}
+//         className={classes}> 
+//         {props.label}
+//     </button>
+
+//     )
+// }
